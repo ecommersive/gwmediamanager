4aeeb22848e65854bcbcc6e0fcdddb34f36ed1c9
Prajeevan <powsolid@gmail.com>
2024-04-16 14:01:43 -0400

    k

--- api.config.js
+++ api.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	name: "gwadmin", // Name of your application
-	script: "api/index.ts", // Entry point of your application
+	script: "api/index.js", // Entry point of your application
 	interpreter: "bun", // Path to the Bun interpreter
 };

98303a00df2a14d4de5ed69fa45b079b5bbf1fbc
richardcommsersive <139133154+richardcommsersive@users.noreply.github.com>
2024-05-27 15:30:28 -0400

    Update ViewList.jsx

--- client/src/Components/ViewTimeModal/ViewList.jsx
+++ client/src/Components/ViewTimeModal/ViewList.jsx
@@ -8,6 +8,7 @@ const ViewList = ({ currentData, catData, data, modalSearchTerm ,setModalSearchT
     <>
       {((currentData === 'Playlist Schedule' || currentData === 'Ads Schedule') && (catData === 'viewTimes' || catData === 'alterTable')) && (
         <>
+        
           <h1>{currentData === 'Playlist Schedule' ? 'Playlist Schedule' : 'Ads Schedule'}</h1>
           {state === 'Add' && <>
             <SearchInput searchTerm={modalSearchTerm} setSearchTerm={setModalSearchTerm}/>
@@ -17,9 +18,9 @@ const ViewList = ({ currentData, catData, data, modalSearchTerm ,setModalSearchT
                 modalFilteredData.filter(modalItem => !itemExists(modalItem.FileName)).map((modalItem, index) => (
                   <div key={index}>
                     <span>{modalItem.FileName}</span>
-                    {/* <button onClick={(event) => handleAddToSet(event, modalItem.FileName)}>
+                    <button>
                       Add
-                    </button> */}
+                    </button>
                   </div>
                 ))
               ) : (
@@ -31,17 +32,16 @@ const ViewList = ({ currentData, catData, data, modalSearchTerm ,setModalSearchT
           </> }
           {data && (
             <div>
-              <p>
-                {currentData === 'Playlist Schedule' ? `Duration of Playlist ${data.folder}` : `Duration of Ads ${data.folder}`} : {new Date(data.startDate).toLocaleDateString()} - {new Date(data.endDate).toLocaleDateString()}
-              </p>
               {state === '' && catData === 'alterTable' && (
                 <>
+                  <p>
+                    {currentData === 'Playlist Schedule' ? `Duration of Playlist ${data.folder}` : `Duration of Ads ${data.folder}`} : {new Date(data.startDate).toLocaleDateString()} - {new Date(data.endDate).toLocaleDateString()}
+                  </p>
                   <h1>Alter {currentData === 'Playlist Schedule' ? ` Playlist ${data.folder}` : ` Ads ${data.folder}`}</h1>
                   <div style={{ display: 'flex', margin: '0 10px' }}>
                     <button className='action-button' style={{ margin: '0 10px' }} onClick={() => setState('Add')}>Add</button>
                     <button className='action-button' style={{ margin: '0 10px' }} onClick={() => setState('Delete')}>Delete</button>
-                    <button className='action-button' style={{ margin: '0 10px' }} onClick={() => setState('MoveUp')}>Move Up</button>
-                    <button className='action-button' style={{ margin: '0 10px' }} onClick={() => setState('MoveDown')}>Move Down</button>
+                    <button className='action-button' style={{ margin: '0 10px' }} onClick={() => setState('Move')}>Move</button>
                   </div>
                 </>
               )}
@@ -51,14 +51,24 @@ const ViewList = ({ currentData, catData, data, modalSearchTerm ,setModalSearchT
                   <ul>
                     {data.items.map((item, index) => (
                       <li key={index}>
+                        
                         {item}
+                        {state === 'Delete' && (
+                          <button className='action-button' onClick={() => console.log('Delete', item)}>Delete</button>
+                        )}
+                        {state === 'Move' && (
+                          <>
+                            {index !== 0 && <button className='action-button' onClick={() => console.log('MoveUp', item)}>Up</button>}
+                            {index !== data.items.length - 1 && <button className='action-button' onClick={() => console.log('MoveDown', item)}>Down</button>}
+                          </>
+                        )}
                       </li>
                     ))}
                   </ul>
                 </div>
               )}
               {
-                (state === 'Add' || state === 'Delete' || state === 'MoveUp' || state === 'MoveDown') && <button className='action-button' onClick={() => setState('')}>Done Editing</button>
+                (state === 'Add' || state === 'Delete' || state === 'Move') && <button className='action-button' onClick={() => setState('')}>Save</button>
               }
             </div>
           )}
